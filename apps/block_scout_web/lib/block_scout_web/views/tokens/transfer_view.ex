cf781e89bbc4f361efdb37b82dbdf5c607ee0dcf
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-11-15 11:28:34 -0200

    function for next page link

--- apps/block_scout_web/lib/block_scout_web/views/tokens/transfer_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/tokens/transfer_view.ex
@@ -2,4 +2,16 @@ defmodule BlockScoutWeb.Tokens.TransferView do
   use BlockScoutWeb, :view
 
   alias BlockScoutWeb.Tokens.OverviewView
+
+  def next_page_button(conn, contract, next_page_params) do
+    next_page_url = token_transfer_path(conn, :index, contract, Map.delete(next_page_params, "type"))
+
+    """
+      <div class="button button-secondary button-small float-right mt-4" data-token-transfers-next-page data-next-page-link=#{
+      next_page_url
+    }>
+        #{gettext("Older")}
+      </div>
+    """
+  end
 end
